3903b760a82c86ed5b208e939bdf59cd76743022
sshwsfc <sshwsfc@gmail.com>
2013-08-07 11:23:49 +0800

    update app name to host.

--- host/adminx.py
+++ host/adminx.py
@@ -12,8 +12,8 @@ class MainDashboard(object):
     widgets = [
         [
             {"type": "html", "title": "Test Widget", "content": "<h3> Welcome to Xadmin! </h3><p>Github: <a href='https://github.com/sshwsfc/django-xadmin' target='_blank'>https://github.com/sshwsfc/django-xadmin</a></p><p>Join Online Group: <br/>QQ Qun : 282936295</p>"},
-            {"type": "chart", "model": "hosts.accessrecord", 'chart': 'user_count', 'params': {'_p_date__gte': '2013-01-08', 'p': 1, '_p_date__lt': '2013-01-29'}},
-            {"type": "list", "model": "hosts.host", 'params': {'o':'-guarantee_date'}},
+            {"type": "chart", "model": "host.accessrecord", 'chart': 'user_count', 'params': {'_p_date__gte': '2013-01-08', 'p': 1, '_p_date__lt': '2013-01-29'}},
+            {"type": "list", "model": "host.host", 'params': {'o':'-guarantee_date'}},
         ],
         [
             {"type": "qbutton", "title": "Quick Start", "btns": [{'model': Host}, {'model':IDC}, 
